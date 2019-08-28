eabfbc8a0469e9a9a7b18d4c7024622822b98d79
bsamm <brandonc.sammons@gmail.com>
2019-08-27 20:42:21 -0700

    get it working with rels

--- neo-network-docs.rb
+++ neo-network-docs.rb
@@ -17,21 +17,30 @@ class NeoNetworkDocs
     neo4j_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new('http://neo4j:password@localhost:7474')
     Neo4j::ActiveBase.on_establish_session { Neo4j::Core::CypherSession.new(neo4j_adaptor) }
 
-    Neo4j::ActiveBase.current_session.query("CREATE CONSTRAINT ON (n:App) ASSERT n.uuid IS UNIQUE")
+    query("MATCH (n) DETACH DELETE n")
+    query("CREATE CONSTRAINT ON (n:App) ASSERT n.uuid IS UNIQUE")
 
     data.each do |network|
+
       network.each do |app|
-        App.create(name: app['name'])
-        app['relationships'].each do |rel|
-          # create relationship with label and create node, if it doesn't exist yet
-          rel_name = rel.keys[0]
-          rel.values[0].each do |node|
-            # puts app['name']
-            # puts rel_name
-            # puts node
+        App.find_or_create_by!(
+          name: app['name']
+        )
+      end
+
+      network.each do |app|
+        source_app = App.find_or_create_by!(name: app['name'])
+        if app.key?("relationships")
+          app['relationships'].each do |rel|
+            rel_name = rel.keys[0]
+            rel.values[0].each do |target_app_name|
+              target_app = App.find_or_create_by!(name: target_app_name)
+              ExportsDataTo.create(from_node: source_app, to_node: target_app)
+            end
           end
         end
       end
+
     end
   end
 
@@ -43,16 +52,31 @@ class NeoNetworkDocs
     data
   end
 
+  def self.query(cypher_query)
+    Neo4j::ActiveBase.current_session.query(cypher_query)
+  end
 end
 
 class App
   include Neo4j::ActiveNode
   include Neo4j::Timestamps
 
+  has_many :out, :exports, model_class: :App, rel_class: :ExportsDataTo
+  has_many :in, :imports, model_class: :App, rel_class: :ImportsDataFrom
+
   property :name, type: String
 end
 
-class Rel
+class ExportsDataTo
   include Neo4j::ActiveRel
-  include Neo4j::Timestamps
+
+  from_class :App
+  to_class :App
+end
+
+class ImportsDataFrom
+  include Neo4j::ActiveRel
+
+  from_class :App
+  to_class :App
 end
